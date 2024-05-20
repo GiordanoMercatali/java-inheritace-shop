86325cfaf33498906ef40eeebe7cc9c2f1fae025
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2024-05-20 15:09:13 +0200

    add: smartphone class

--- src/main/java/org/lessons/java/Shop/Main.java
+++ src/main/java/org/lessons/java/Shop/Main.java
@@ -4,19 +4,26 @@ import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
-        Product apple = new Product("Apple", "Una mela al giorno toglie il medico di torno", new BigDecimal(1.22), new BigDecimal(0.22));
-        // String name = apple.getName();
-        // System.out.println("Product name: " + name);
-        String codename = apple.getCodeName();
-        System.out.println("Product: " + codename);
-        String desc = apple.getDesc();
-        System.out.println(desc);
-        BigDecimal price = apple.getPrice();
-        System.out.println("Original price: " + price + "$");
-        BigDecimal taxedPrice = apple.getTaxedPrice(price);
-        System.out.println("Price with taxes: " + taxedPrice + "$");
-        // String code = apple.getCode();
-        // System.out.println("Product code: " + code);
+        // Product omen = new Product("HP Omen 16", "Gaming laptop", new BigDecimal(1122), new BigDecimal(0.22));
+        // // String name = apple.getName();
+        // // System.out.println("Product name: " + name);
+        // String codename = omen.getCodeName();
+        // System.out.println("Product: " + codename);
+        // String desc = omen.getDesc();
+        // System.out.println(desc);
+        // BigDecimal price = omen.getPrice();
+        // System.out.println("Original price: " + price + "$");
+        // BigDecimal taxedPrice = omen.getTaxedPrice(price);
+        // System.out.println("Price with taxes: " + taxedPrice + "$");
+        // // String code = apple.getCode();
+        // // System.out.println("Product code: " + code);
         
+        Smartphone iphone = new Smartphone("iPhone 15", "This year's iPhone. It'll become obsolete in less than a year...", new BigDecimal(1399), new BigDecimal(0.22));
+        System.out.println(iphone.getCodeName());
+        System.out.println(iphone.getDesc());
+        System.out.println(iphone.getPrice() + "$");
+        System.out.println(iphone.getTaxedPrice(iphone.getPrice()) + "$");
+        System.out.println(iphone.getRandomImei());
+        System.out.println(iphone.getMaxStorage() + "GB");
     }
 }
---
+++ src/main/java/org/lessons/java/Shop/Smartphone.java
@@ -0,0 +1,27 @@
+package org.lessons.java.Shop;
+
+import java.math.BigDecimal;
+import java.util.Random;
+
+class Smartphone extends Product{
+
+    private int imei;
+    private int storage;
+    private int[] storageOptions = {128, 256, 512, 1024};
+
+    public Smartphone(String name, String description, BigDecimal price, BigDecimal vat){
+        super(name, description, price, vat);
+        // this.imei = imei;
+        // this.storage = storage;
+        this.imei = getRandomImei();
+        this.storage = getMaxStorage();
+    }
+
+    int getRandomImei(){
+        return new Random().nextInt(100000, 1000000);
+    }
+
+    int getMaxStorage(){
+        return storageOptions[new Random().nextInt(0, 3)];
+    }
+}
